0432e4961e501fbe75f7816981ab20510a67bf27
Jeroen van der Laan <jeroen@jground.nl>
2011-10-23 20:50:25 +0200

    Added functionality:
    - Sidebar (created sidebar by rendering an action in the main layout)
    - Created sqlite database file for testing.
    - Setup config.yml to connect to the sqlite file.
    - Created sidebar content with a seperate action that gets the connection and load the table information using the schema manager
    - Fixed about page. (no crash)

--- src/Ngem/EngineBundle/Controller/DefaultController.php
+++ src/Ngem/EngineBundle/Controller/DefaultController.php
@@ -26,4 +26,16 @@ class DefaultController extends Controller
       return array();
     }
 
+    /**
+     * @Template()
+     */
+    public function sidebarAction()
+    {
+      /** @var $db \Doctrine\DBAL\Connection */
+      $db = $this->get('doctrine.dbal.default_connection');
+      $tables = $db->getSchemaManager()->listTables();
+//      var_dump($tables); die();
+      return array("tables" => $tables);
+    }
+
 }
